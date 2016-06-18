6247f079f80bd753e65405e678e8aebfe0ad5639
Ben Rachbach <brachbach@gmail.com>
2016-06-18 16:15:24 -0700

    Fix issue noted on last commit and implement bcrypt for signup

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -1,24 +1,32 @@
 const passport = require('passport');
+const bcrypt = require('bcrypt-nodejs');
 
-var dummyCreateUser = (user, cb) => {
+const dummyCreateUser = (email, hashedPassword, cb) => {
   console.log('user created!');
   console.log(cb);
   cb({"email": "ben@gmail.com", "password": "abc123"});
 };
 
+const generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
 exports.signin = passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login'}),
   
 exports.signup = (req, res) => {
-  let user = req.body;
-  console.log('user:', user);
-  dummyCreateUser(user,
-
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = generateHash(password);
+  console.log('hashedPassword:', hashedPassword);
+  dummyCreateUser(email, hashedPassword, user => {
     req.login(user, function(err) {
         if (err) { return next(err); }
+        console.log('user to login with after signup:', user);
         console.log('logged in and redirecting');
         return res.redirect('/');
-      })
-    );
+      });
+    }
+  );
 };
 
 
