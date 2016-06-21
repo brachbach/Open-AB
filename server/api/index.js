c18a88e28f48102c576d9a46e8a8ce4e061f8489
Ben Rachbach <brachbach@gmail.com>
2016-06-21 13:44:41 -0700

    Finalizing alignment of merge

--- server/api/index.js
+++ server/api/index.js
@@ -9,31 +9,16 @@ const app = express();
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 8080;
 
-
 app.use(express.static(`${__dirname}/../../client`));
 app.use(bodyParser.urlencoded({ extended: true })); // make sure this is necessary
 app.use(bodyParser.json());
 app.use(cookieParser('cheese'));
 authConfig(app);
-
 authRoutes(app);
 
 app.get('/', (req, res) => {
   res.send('serving up static files!');
 });
-
-// test routes for DB
-const analytics = require('./analytics/controllers');
-app.get('/api/results', analytics.getAll);
-app.post('/api/createTest', analytics.createTest);
-
-const auth = require('./auth/controllers');
-app.post('/api/email', auth.checkEmail);
-app.post('/api/signup', auth.signUp);
-app.post('/api/signin', auth.signIn);
-// end test route for DB
-authRoutes(app);
-
 
 app.listen(port, (err) => {
   if (err) {
