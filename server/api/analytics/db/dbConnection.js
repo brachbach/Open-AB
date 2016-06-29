fdea1b7c9f372e29d4609d68a6b35551dfb6fb2d
Ben Rachbach <brachbach@gmail.com>
2016-06-29 14:11:27 -0700

    Write tests and events to the database

--- server/api/analytics/db/dbConnection.js
+++ server/api/analytics/db/dbConnection.js
@@ -1,10 +1,11 @@
 const cfg = require('../config');
 const pg = require('pg');
+const pgp = require('pg-promise')();
 
 const clientLink = `postgres://${cfg.db.host}:${cfg.db.port}/${cfg.db.dbName}`;
 
 // callback that allows for connection to the database
-module.exports = {
+module.exports.pg = {
   query: (text, cb) => {
     pg.connect(clientLink, (err, client, done) => {
       client.query(text, (error, result) => {
@@ -14,3 +15,5 @@ module.exports = {
     });
   },
 };
+
+module.exports.pgp = pgp(clientLink);
