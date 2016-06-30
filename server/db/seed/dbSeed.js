a6118e08f40fe85f6ba9838fb4b96e2c16e7f5cc
Ben Rachbach <brachbach@gmail.com>
2016-06-30 15:58:07 -0700

    Using async series for some reason

--- server/db/seed/dbSeed.js
+++ server/db/seed/dbSeed.js
@@ -1,8 +1,9 @@
-const dbQry = require('./db/dbQueries');
 const async = require('async');
 
 const generateEvents = require('../../api/analytics/stats/generateEvents.js');
 const clientData = require('../../api/clientData.js');
+const dbQry = require('./db/dbQueries');
+const analyticsQry = require('../../api/analytics/db/dbQueries.js')
 
 const clientHardcodedData = [
   {
@@ -21,11 +22,46 @@ const page = client.pages[0];
 
 const insertClientHardcodedData = cb => {
   dbQry.insertClient(client.email, client.password, () => {
+    console.log('inserting client');
     dbQry.insertPage(page.pageName, client.email, () => {
+      console.log('inserting page');
       cb();
     });
   });
 };
 
-insertClientHardcodedData(() => 'success');
+const insertClientData = () => {
+  async.series([
+    CB => {
+      async.each(clientData, 
+
+        (testData, cb) => {
+          analyticsQry.createTest(testData, client.email, 
+            (err, result) => {
+              if (err) {
+                console.log(err);
+              } else {
+                cb();
+              }
+            }
+          );
+        },
+
+        (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            CB();
+          }
+        }
+      );
+    };
+  ],
+
+  (err, result) => {
+    console.log(err, result);
+  });
+};
+
+insertClientHardcodedData(insertClientData);
 
