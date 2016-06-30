34ca4b0dc4d8810ca7dde141930aa046cd4ddf42
Ben Rachbach <brachbach@gmail.com>
2016-06-30 16:45:47 -0700

    Insert visits for one version

--- server/db/seed/dbSeed.js
+++ server/db/seed/dbSeed.js
@@ -3,7 +3,8 @@ const async = require('async');
 const generateEvents = require('../../api/analytics/stats/generateEvents.js');
 const clientData = require('../../api/clientData.js');
 const dbQry = require('./db/dbQueries');
-const analyticsQry = require('../../api/analytics/db/dbQueries.js')
+const analyticsQry = require('../../api/analytics/db/dbQueries.js');
+const listeningQry = require('../../../server/listening/events/db/dbQueries.js');
 
 const clientHardcodedData = [
   {
@@ -20,17 +21,19 @@ const clientHardcodedData = [
 const client = clientHardcodedData[0];
 const page = client.pages[0];
 
-const insertClientHardcodedData = cb => {
+const eventsForAllTests = generateEvents.generateDataForMultipleTestsWithDefaultParams();
+
+const insertClientHardcodedData = callback => {
   dbQry.insertClient(client.email, client.password, () => {
     console.log('inserting client');
     dbQry.insertPage(page.pageName, client.email, () => {
       console.log('inserting page');
-      cb();
+      callback();
     });
   });
 };
 
-const insertClientData = () => {
+const insertClientData = callback => {
   async.each(clientData,
 
     (testData, cb) => {
@@ -51,11 +54,39 @@ const insertClientData = () => {
         console.log(err);
       } else {
         console.log('inserted tests');
+        callback();
+      }
+    }
+  );
+};
+
+const insertVersionVisits = (visitsArray) => {
+  async.each(visitsArray,
+
+    (visit, cb) => {
+      console.log('visit', visit);
+      visit.versionId = 1;
+      listeningQry.hearVisit(visit,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            cb();
+          }
+        }
+      );
+    },
+
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('inserted visits');
         return;
       }
     }
   );
 };
 
-insertClientHardcodedData(insertClientData);
+insertClientHardcodedData(() => insertClientData(() => insertVersionVisits(eventsForAllTests[0].data.aVisitsData)));
 
