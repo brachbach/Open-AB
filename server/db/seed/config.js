322808f1accfeda2131fdbcd0664a41ac35ff479
Ben Rachbach <brachbach@gmail.com>
2016-07-02 11:58:30 -0700

    Deploy api server

--- server/db/seed/config.js
+++ server/db/seed/config.js
@@ -1,7 +1,15 @@
-module.exports = {
-  db: {
-    host: 'localhost',
-    port: 5432,
-    dbName: process.env.NODE_ENV === 'test' ? 'test' : 'openab',
-  },
-};
+module.exports = (() => {
+  const env = process.env.NODE_ENV;
+  if (env === 'test') {
+    return 'postgres://localhost:5432/test';
+  } else if (env === 'deployed') {
+    return {
+      user: 'openab',
+      database: 'openab',
+      password: '&$X7zs#GH9Z7',  // TODO: don't publish this to the world!
+      host: 'openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com',
+      port: 5432,
+    };
+  }
+  return 'postgres://localhost:5432/openab';
+})();
