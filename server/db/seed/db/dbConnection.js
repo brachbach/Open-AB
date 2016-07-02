d7ce53f60721f85641c453513a983463e4db1ba6
Ben Rachbach <brachbach@gmail.com>
2016-07-01 22:42:09 -0700

    Added console. error

--- server/db/seed/db/dbConnection.js
+++ server/db/seed/db/dbConnection.js
@@ -7,6 +7,7 @@ const clientLink = 'pg://openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.a
 module.exports = {
   query: (text, cb) => {
     pg.connect(clientLink, (err, client, done) => {
+      console.error(err);
       client.query(text, (err, result) => {
         done();
         cb(err, result);
