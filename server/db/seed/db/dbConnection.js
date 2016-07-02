cd36a9b443ee5597196411a9536e0e6ffc98e0df
Ben Rachbach <brachbach@gmail.com>
2016-07-01 23:02:02 -0700

    Switch back to our db

--- server/db/seed/db/dbConnection.js
+++ server/db/seed/db/dbConnection.js
@@ -1,7 +1,9 @@
 const cfg = require('../config');
 const pg = require('pg');
 
-const clientLink = 'pg://farmConnect:9lXJfi7dD&e0@farmconnect1.cgvp1wmwkltn.us-west-2.rds.amazonaws.com:5432/farmdata';
+const clientLink = 'pg://openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com:5432/openab';
+
+//'pg://farmConnect:9lXJfi7dD&e0@farmconnect1.cgvp1wmwkltn.us-west-2.rds.amazonaws.com:5432/farmdata';
 
 // callback that allows for connection to the database
 module.exports = {
