0d3f3be3a0e7b6242f458e7a4d63b68bf12a8527
Ben Rachbach <brachbach@gmail.com>
2016-07-01 22:47:29 -0700

    remove pg

--- server/db/seed/db/dbConnection.js
+++ server/db/seed/db/dbConnection.js
@@ -1,7 +1,7 @@
 const cfg = require('../config');
 const pg = require('pg');
 
-const clientLink = 'pg://openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com:5432/openab';
+const clientLink = 'openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com:5432/openab';
 
 // callback that allows for connection to the database
 module.exports = {
