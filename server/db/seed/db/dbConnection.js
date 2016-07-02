515ef927373fc71b81ce4d3ae968fc0fa7582801
Ben Rachbach <brachbach@gmail.com>
2016-07-01 22:56:13 -0700

    Try to connect to farmConnect db

--- server/db/seed/db/dbConnection.js
+++ server/db/seed/db/dbConnection.js
@@ -1,7 +1,7 @@
 const cfg = require('../config');
 const pg = require('pg');
 
-const clientLink = 'openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com:5432/openab';
+const clientLink = 'pg://farmConnect:9lXJfi7dD&e0@farmconnect1.cgvp1wmwkltn.us-west-2.rds.amazonaws.com:5432/farmdata';
 
 // callback that allows for connection to the database
 module.exports = {
