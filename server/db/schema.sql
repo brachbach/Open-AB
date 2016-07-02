307aee35b75101626ac6f631f512b3d472eef38e
Ben Rachbach <brachbach@gmail.com>
2016-07-02 12:12:21 -0700

    Fix seed script

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,50 +6,7 @@ CREATE DATABASE test;
 
 \c openab
 
-DROP TABLE IF EXISTS clients, pages, tests, versions, visits, clicks
-
-CREATE TABLE clients (
- id SERIAL PRIMARY KEY,
- email VARCHAR(50) UNIQUE,
- password VARCHAR(255)
-);
-
-CREATE TABLE pages (
-  id SERIAL PRIMARY KEY,
-  client_id integer REFERENCES clients (id),
-  name VARCHAR(50)
-);
-
-CREATE TABLE tests (
-  id SERIAL PRIMARY KEY,
-  page_id integer REFERENCES pages (id),
-  name VARCHAR(50),
-  result_a integer,
-  result_b integer,
-  uniqueid VARCHAR(250)
-);
-
-CREATE TABLE versions (
-  id SERIAL PRIMARY KEY,
-  test_id integer REFERENCES tests (id),
-  ab VARCHAR(50),
-  url VARCHAR(250),
-  domlocation VARCHAR(50)
-);
-
-CREATE TABLE visits (
-  id SERIAL PRIMARY KEY,
-  version_id integer REFERENCES versions (id),
-  ipaddress VARCHAR(50),
-  time bigint
-);
-
-CREATE TABLE clicks (
-  id SERIAL PRIMARY KEY,
-  version_id integer REFERENCES versions (id),
-  ipaddress VARCHAR(50),
-  time bigint
-);
+b
 
 \c test
 
