670cdb9e3c63e203d7250b37e9a14e3ba209a7a1
Ben Rachbach <brachbach@gmail.com>
2016-07-01 22:38:18 -0700

    Switch to farm connect db connection

--- server/db/seed/db/dbConnection.js
+++ server/db/seed/db/dbConnection.js
@@ -1,22 +1,17 @@
 const cfg = require('../config');
 const pg = require('pg');
 
-const clientLink = 'postgress://openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com:5432/openab';
+const clientLink = 'pg://openab:&$X7zs#GH9Z7@openab.cj3poakpg8kc.us-west-2.rds.amazonaws.com:5432/openab';
 
 // callback that allows for connection to the database
 module.exports = {
   query: (text, cb) => {
     pg.connect(clientLink, (err, client, done) => {
-      if (client) {
-        client.query(text, (error, result) => {
-          done();
-          cb(error, result);
-        });
-      } else {
-        err.message = 'Database connection refused';
-        cb(err, null);
-      }
+      client.query(text, (err, result) => {
+        done();
+        cb(err, result);
+      });
     });
-  },
+  }
 };
 
