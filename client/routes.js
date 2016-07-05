af6a5ee240a020b7f8a2605d775c648fc2b6d345
Ben Rachbach <brachbach@gmail.com>
2016-07-04 19:33:48 -0700

    Create landing page

--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Dashboard from './containers/Dashboard';
+import LandingPage from './components/LandingPage';
 
 
 export default (
-  <Route path="/" component={Dashboard} />
+  [
+    <Route path="/" component={LandingPage} />,
+    <Route path="/Dashboard" component={Dashboard} />,
+  ]
 );
